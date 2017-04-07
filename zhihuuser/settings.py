ef8e24e09a9ff2f42fb11e07d94fa8eadc429d0a
Germey <cqc@cuiqingcai.com>
2017-04-07 16:52:57 +0800

    remove redis pipeline

--- zhihuuser/settings.py
+++ zhihuuser/settings.py
@@ -75,7 +75,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'zhihuuser.pipelines.MongoPipeline': 300,
-    'scrapy_redis.pipelines.RedisPipeline': 301
+    # 'scrapy_redis.pipelines.RedisPipeline': 301
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
